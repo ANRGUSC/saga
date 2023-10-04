4928a45581193c081eefb930be72a616ed98d7a1
EbsHirani <hiraniebrahim13@gmail.com>
2023-10-04 16:37:45 -0700

    Updated main.py in examples

--- examples/hbmct/main.py
+++ examples/hbmct/main.py
@@ -1,6 +1,7 @@
 import itertools
 from saga.schedulers.hbmct import HbmctScheduler
 from saga.utils.draw import draw_gantt, draw_network, draw_task_graph
+
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,12 +10,14 @@ import pathlib
 
 thisdir = pathlib.Path(__file__).parent.absolute()
 
+
 def main():
+    # greate a diamond-task graph with random task weights between 0 and 1
     task_graph = nx.DiGraph()
     task_graph.add_nodes_from([1, 2, 3, 4])
     task_graph.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
     for node in task_graph.nodes:
-        task_graph.nodes[node]['weight'] = np.random.uniform(low = 2, high= 5)
+        task_graph.nodes[node]['weight'] = np.random.rand()
     for edge in task_graph.edges:
         task_graph.edges[edge]['weight'] = np.random.rand()
 
@@ -24,7 +27,7 @@ def main():
     for node in network.nodes:
         network.nodes[node]['weight'] = np.random.rand()
     for (src, dst) in itertools.product(network.nodes, network.nodes):
-        # print(f"Adding edge {src} -> {dst}")
+        print(f"Adding edge {src} -> {dst}")
         if src != dst:
             network.add_edge(src, dst, weight=np.random.rand())
         else:
@@ -32,7 +35,8 @@ def main():
 
     scheduler = HbmctScheduler()
     schedule = scheduler.schedule(network, task_graph)
-    # print(schedule)
+
+    # draw the task graph
     ax: plt.Axes = draw_task_graph(task_graph)
     ax.set_title('Task Graph')
     fig = ax.get_figure()
@@ -42,7 +46,13 @@ def main():
     ax: plt.Axes = draw_network(network)
     ax.set_title('Network')
     fig = ax.get_figure()
-    fig.savefig(thisdir / 'network.png')
+    fig.savefig(thisdir / 'network_rand.png')
+
+    # draw the schedule
+    fig: Figure = draw_gantt(schedule)
+    fig.update_layout(title='Schedule')
+    fig.write_image(str(thisdir / 'schedule_rand.png'))
+
 
 if __name__ == '__main__':
     main()
