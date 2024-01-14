Lookahead fixed

saga/general/InsertTask/lookahead_insert.py
@@ -9,24 +9,30 @@ from .utils import insert
 import numpy as np
 import copy
 
-def get_k_depth_children(task_graph: nx.DiGraph, task_name: Hashable, k: int) -> nx.DiGraph:
+def get_k_depth_children(task_graph: nx.DiGraph, task_name: Hashable, k: int, task_schedule) -> nx.DiGraph:
     new_task_graph = nx.DiGraph()
     new_task_graph.add_node(task_name)
+    new_task_graph.nodes[task_name]["weight"] = task_graph.nodes[task_name]["weight"]
     def recurse(task_name, k):
         if k == 0:
             return
         for child in task_graph.successors(task_name):
-            if child not in new_task_graph.nodes:
-                new_task_graph.add_node(child)
-                new_task_graph.nodes[child]["weight"] = task_graph.nodes[child]["weight"]
-            new_task_graph.add_edge(task_name, child)
-            new_task_graph.edges[task_name, child]["weight"] = task_graph.edges[task_name, child]["weight"]
-            recurse(child, k-1)
+            if set(task_graph.predecessors(child)).issubset(
+                    task_schedule.keys()):
+                if child not in new_task_graph.nodes:
+                    new_task_graph.add_node(child)
+                    new_task_graph.nodes[child]["weight"] = task_graph.nodes[child]["weight"]
+                if (task_name, child) not in new_task_graph.edges:
+                    new_task_graph.add_edge(task_name, child)
+                    new_task_graph.edges[task_name, child]["weight"] = task_graph.edges[task_name, child]["weight"]
+                recurse(child, k-1)
     recurse(task_name, k)
     return new_task_graph
+
 class LookAheadInsert(ABC):
     
-    def __init__(self, k=1,
+    def __init__(self,
+                k=1,
                 scheduler = None) -> None:
         self.k = k
         if scheduler is None:
@@ -44,7 +50,7 @@ class LookAheadInsert(ABC):
         task_schedule: Dict[Hashable, Task],
         task_name: Hashable,
         priority: int,
-        ) -> None:  
+        ) -> None:
         
         if task_graph.out_degree(task_name) == 0:
             self.scheduler.insert_task(network, task_graph, runtimes, commtimes, comp_schedule, task_schedule, task_name, priority)
@@ -53,13 +59,14 @@ class LookAheadInsert(ABC):
 
         min_finish_time = np.inf
         best_node = None
-        _task_graph = get_k_depth_children(task_graph, task_name, self.k)
+        _task_graph = get_k_depth_children(task_graph, task_name, self.k, task_schedule)
         for node in network.nodes:
             _task_schedule = copy.deepcopy(task_schedule)
             _comp_schedule = copy.deepcopy(comp_schedule)
             insert(task_graph, runtimes, commtimes, node, task_name, _comp_schedule, _task_schedule)
-            _task_schedule = self.scheduler.schedule(network, task_graph, _comp_schedule, _task_schedule, runtimes = runtimes, commtimes = commtimes)
-            finish_time = max([_task_schedule[node][-1].end for node in _task_schedule if _task_schedule[node]])
+            _comp_schedule = self.scheduler.schedule(network, _task_graph, _comp_schedule, _task_schedule, runtimes = runtimes, commtimes = commtimes)
+            finish_time = max(_task_schedule[task_name].end for task_name in _task_graph.nodes)
+
             if finish_time < min_finish_time:
                 min_finish_time = finish_time
                 best_node = node