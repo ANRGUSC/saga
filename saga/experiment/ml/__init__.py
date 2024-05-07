78f5369d2cfdb70218d76924c2a0061765b0d93a
Jared Coleman <jaredraycoleman@gmail.com>
2024-05-06 17:18:07 -0700

    Updates dataset scripts

--- saga/experiment/ml/__init__.py
+++ saga/experiment/ml/__init__.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import json
+import pathlib
 from typing import Dict, List
 from saga.data import serialize_graph, deserialize_graph
 from saga.experiment.benchmarking.parametric.components import UpwardRanking, GreedyInsert, ParametricScheduler
@@ -10,17 +11,16 @@ import os
 
 from saga.experiment import datadir, resultsdir, outputdir
 
-def prepare_dataset():
-    LEVELS = 3
-    BRANCHING_FACTOR = 2
-    NUM_NODES = 10
-    NUM_PROBLEMS = 100
-
+def prepare_dataset(levels: int = 3,
+                    branching_factor: int = 2,
+                    num_nodes: int = 10,
+                    num_problems: int = 100,
+                    savepath: pathlib.Path = None):
     dataset = []
-    for i in range(NUM_PROBLEMS):
-        print(f"Progress: {i+1/NUM_PROBLEMS*100:.2f}%" + " " * 10, end="\r")
-        task_graph = add_random_weights(get_branching_dag(levels=LEVELS, branching_factor=BRANCHING_FACTOR))
-        network = add_random_weights(get_network(num_nodes=NUM_NODES))
+    for i in range(num_problems):
+        print(f"Progress: {(i+1)/num_problems*100:.2f}%" + " " * 10, end="\r")
+        task_graph = add_random_weights(get_branching_dag(levels=levels, branching_factor=branching_factor))
+        network = add_random_weights(get_network(num_nodes=num_nodes))
 
         topological_sorts = list(nx.all_topological_sorts(task_graph))
         for topological_sort in topological_sorts:
@@ -48,26 +48,38 @@ def prepare_dataset():
 
     print("Progress: 100.00%")
 
-    savedir = datadir / "ml"
-    savedir.mkdir(exist_ok=True, parents=True)
-    dataset_path = savedir / "data.json"
-    dataset_path.write_text(json.dumps(dataset, indent=4))
+    savepath.parent.mkdir(parents=True, exist_ok=True)
+    savepath.write_text(json.dumps(dataset, indent=4))
 
-def load_dataset(cache: bool = True) -> List[Dict]:
+def load_dataset(cache: bool = True,
+                 task_graph_levels: int = 3,
+                 task_graph_branching_factor: int = 2,
+                 network_nodes: int = 10,
+                 num_instances: int = 100) -> List[Dict]:
     """Load the dataset from disk and deserialize the graphs.
     
     Args:
         cache (bool, optional): Whether to cache the dataset. Defaults to True.
+        task_graph_levels (int, optional): Number of levels in the task graph. Defaults to 3.
+        task_graph_branching_factor (int, optional): Branching factor of the task graph. Defaults to 2.
+        network_nodes (int, optional): Number of nodes in the network. Defaults to 10.
+        num_instances (int, optional): Number of instances to generate. Defaults to 100.
 
     Returns:
         List[Dict]: List of problem instances, each containing a task graph, network, topological sort, and makespan
             of HEFT on the problem instance with the given topological sort.
     """
-    savedir = datadir / "ml"
+    savedir = datadir / "ml" / f"l{task_graph_levels}_b{task_graph_branching_factor}_n{network_nodes}_i{num_instances}"
     dataset_path = savedir / "data.json"
     if not dataset_path.exists() or not cache:
-        print("Dataset not found. Generating dataset...")
-        prepare_dataset()
+        print("Generating dataset...")
+        prepare_dataset(
+            levels=task_graph_levels,
+            branching_factor=task_graph_branching_factor,
+            num_nodes=network_nodes,
+            num_problems=num_instances,
+            savepath=dataset_path
+        )
 
     print("Loading dataset...")
     dataset = json.loads(dataset_path.read_text())
