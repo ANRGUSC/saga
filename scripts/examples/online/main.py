7c9e59a88ded857d222a160f0b7d8c87456a0852
Jared Coleman <jaredraycoleman@gmail.com>
2025-01-14 15:44:59 -0800

    Adds starter code for online scheduling

--- scripts/examples/online/main.py
+++ scripts/examples/online/main.py
@@ -5,6 +5,24 @@ from saga.scheduler import Scheduler, Task
 from typing import Dict, Hashable, List
 import networkx as nx
 
+def schedule_estimate_to_actual(network: nx.Graph,
+                                task_graph: nx.DiGraph,
+                                schedule_estimate: Dict[Hashable, List[Task]]) -> Dict[Hashable, List[Task]]:
+    """Converts a schedule estimate to a schedule actual.
+    
+    Args:
+        network (nx.Graph): The network graph.
+        task_graph (nx.DiGraph): The task graph.
+        schedule_estimate (Dict[Hashable, List[Task]]): The estimated schedule.
+
+    Returns:
+        Dict[Hashable, List[Task]]: The actual schedule.
+    """
+    schedule_actual = {
+        node: [] for node in network.nodes
+    }
+
+
 class OnlineHeftScheduler(Scheduler):
     def __init__(self):
         self.heft_scheduler = HeftScheduler()
@@ -14,19 +32,21 @@ class OnlineHeftScheduler(Scheduler):
         #   - weight_actual: float
         #   - weight_estimate: float
 
-        schedule: Dict[Hashable, List[Task]] = {
+        schedule_actual: Dict[Hashable, List[Task]] = {
             node: [] for node in network.nodes
         }
         while True:
             # Step 1: set weight attributes to weight_actual if the task is in the schedule
             #         otherwise, set to weight_estimate
 
-            new_schedule = self.heft_scheduler.schedule(network, task_graph, schedule)
+            schedule_estimate = self.heft_scheduler.schedule(network, task_graph, schedule_actual)
+            schedule_actual_hypothetical = schedule_estimate_to_actual(network, task_graph, schedule_estimate)
 
             # Step 2: Get next task to finish based on *actual* execution time
-            # Step 3: Remove all tasks except those already finished or currently running
+            # Step 3: Add task to schedule_actual
             # Step 4: Repeat until there are no more tasks to schedule
 
 
         
-        pass
+def main():
+    pass
