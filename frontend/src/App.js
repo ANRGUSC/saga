323992c6d68864ab4d7dadf20638317551fd2396
sifawangia1 <123137142+sifawangia1@users.noreply.github.com>
2025-04-01 16:24:25 -0700

    create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Using LLM's to Predict the Optimal Algorithm for a Given Schedule</h1>
+    </div>
+  )
+}
+
+export default HomePage
