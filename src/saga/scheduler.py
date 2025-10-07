2fcb6aa9f6d8157e25fb7dfb718c44c2354fcbda
Jared Coleman <jaredraycoleman@gmail.com>
2025-10-07 15:29:57 -0700

    Uses RootModel

--- src/saga/scheduler.py
+++ src/saga/scheduler.py
@@ -1,20 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict, Generator, Hashable, List, Optional, Tuple, Type
 import networkx as nx
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 from pydantic.main import TupleGenerator
 
 class Task(BaseModel):
     """A task."""
-    node: str
-    name: str
+    node: Hashable
+    name: Hashable
     start: float
     end: float
 
-class Schedule(BaseModel):
-    """A schedule."""
-    schedule: Dict[Hashable, List[Task]] = {}
-
+class Schedule(RootModel[Dict[Hashable, List[Task]]]): # pylint: disable=too-few-public-methods
+    """A schedule of nodes with tasks assigned to them."""
     @property
     def makespan(self) -> float:
         """Get the makespan of the schedule.
@@ -22,84 +20,39 @@ class Schedule(BaseModel):
         Returns:
             float: The makespan of the schedule.
         """
-        return max(task.end for tasks in self.schedule.values() for task in tasks if task.end is not None)
-
-    def add_task(self, node: Hashable, task: Task) -> None:
-        """Add a task to the schedule.
-
-        Args:
-            node (Hashable): The node to add the task to.
-            task (Task): The task to add.
-        """
-        if node not in self.schedule:
-            self.schedule[node] = []
-        self.schedule[node].append(task)
-
-    def items(self) -> Generator[Tuple[Hashable, List[Task]], None, None]:
-        """
-        Get the items of the schedule.
+        return max(task.end for tasks in self.root.values() for task in tasks if task.end is not None)
+    
+    def __iter__(self) -> Generator[Tuple[Hashable, List[Task]], None, None]: # type: ignore
+        """Iterate over the schedule.
 
         Yields:
-            Generator[Tuple[Hashable, List[Task]], None, None]: The items of the schedule.
+            Generator[Tuple[Hashable, List[Task]], None, None]: A generator of tuples of node and list of tasks.
         """
-        yield from self.schedule.items()
+        yield from self.root.items()
 
-    def __getitem__(self, node: Hashable) -> List[Task]:
-        """
-        Get the tasks for a node.
+    def __getitem__(self, key: Hashable) -> List[Task]: # type: ignore
+        """Get the tasks for a node.
 
         Args:
-            node (Hashable): The node to get the tasks for.
-
+            key (Hashable): The node.
         Returns:
-            List[Task]: The tasks for the node.
-
+            List[Task]: The list of tasks for the node.
         Raises:
             KeyError: If the node is not in the schedule.
         """
-        return self.schedule[node]
+        return self.root[key]
     
-    @property
-    def task_map(self) -> Dict[str, Task]:
+    def add_task(self, task: Task) -> None:
+        """Add a task to the schedule.
+
+        Args:
+            task (Task): The task to add.
         """
-        Get a mapping of task names to tasks.
+        if task.node not in self.root:
+            self.root[task.node] = []
+        self.root[task.node].append(task)
 
-        Returns:
-            Dict[str, Task]: A mapping of task names to tasks.
-        """
-        task_map: Dict[str, Task] = {}
-        for tasks in self.schedule.values():
-            for task in tasks:
-                task_map[task.name] = task
-        return task_map
-
-    def sort_tasks(self) -> None:
-        """Sort the tasks in the schedule by start time."""
-        for tasks in self.schedule.values():
-            tasks.sort(key=lambda task: task.start if task.start is not None else float('inf'))
-
-    def is_sorted(self) -> bool:
-        """Check if the tasks in the schedule are sorted by start time.
-
-        Returns:
-            bool: True if the tasks are sorted, False otherwise.
-        """
-        for tasks in self.schedule.values():
-            if any(tasks[i].start > tasks[i+1].start for i in range(len(tasks)-1)):
-                return False
-        return True
-    
-    def is_valid(self) -> bool:
-        """Check if the schedule is valid in terms of task overlaps.
-
-        Returns:
-            bool: True if the schedule is valid, False otherwise.
-        """
-        for tasks in self.schedule.values():
-            for i in range(len(tasks)-1):
-                if tasks[i].end > tasks[i+1].start:
-                    return False
-        return True
+    # TODO: enforce property that task.node matches the key in the dict
 
 class Scheduler(ABC): # pylint: disable=too-few-public-methods
     """An abstract class for a scheduler."""
