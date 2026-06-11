93149443eb4fc1d6767096ed6a624903de1cd96c
Jason Chamorro <jchamor1@lion.lmu.edu>
2026-06-10 19:41:41 -0700

    processor fix

--- scripts/experiments/throughput_experiment/common.py
+++ scripts/experiments/throughput_experiment/common.py
@@ -7,6 +7,6 @@ resultsdir = thisdir / "results"
 outputdir = thisdir / "output"
 
 num_processors = max(1, (os.cpu_count() or 1) - 3)
-num_processors = min(num_processors,2)
+#num_processors = min(num_processors,2)
 
 os.environ["SAGA_DATA_DIR"] = str(datadir)
